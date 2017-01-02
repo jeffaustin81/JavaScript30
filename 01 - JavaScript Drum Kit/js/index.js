741ad433bc02f4f7e08093945044b0cc33ef3650
Jeff Austin <jeffaustin81@gmail.com>
2017-01-02 11:40:21 -0800

    create js file and create event listener on key event that plays audio dom node when corresponding key is pressed

---
+++ 01 - JavaScript Drum Kit/js/index.js
@@ -0,0 +1,7 @@
+window.addEventListener('keydown', function (e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)  
+  if(!audio) return
+  audio.currentTime = 0
+  audio.play()
+})
