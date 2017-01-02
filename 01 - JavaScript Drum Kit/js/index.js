d067d12fdebec15c3b4676abf181edeb84b61df5
Jeff Austin <jeffaustin81@gmail.com>
2017-01-02 12:12:22 -0800

    Add comments

--- 01 - JavaScript Drum Kit/js/index.js
+++ 01 - JavaScript Drum Kit/js/index.js
@@ -1,13 +1,14 @@
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-  if(!audio) return
-  audio.currentTime = 0
+  if(!audio) return // stop event if audio doesn't exist for key
+  audio.currentTime = 0 // rewind audio so key event always plays audio
   audio.play()
   key.classList.add('playing')
 }
 
 function removeTransition(e) {
+  if(e.propertyName !== 'transform') return // skip if it's not a transform
   this.classList.remove('playing')
 }
 
